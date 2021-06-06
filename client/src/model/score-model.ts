feac0cdc764b1937c9392e5e902366e86f7a65bd
kalish <uri.kalish@gmail.com>
2021-06-06 22:04:28 +0300

    score model

--- client/src/model/score-model.ts
+++ client/src/model/score-model.ts
@@ -1,19 +1,16 @@
 export interface ScoreInfoModel {
-	scoreName: string; //e.g. "Bohemian Rhapsody"
+	scoreTitle: string; //e.g. "Bohemian Rhapsody"
 	scoreCredits: string; //e.g. "Queen - A Night at the Opera - 1975"
 	arrangerName: string; //e.g. "Uri Kalish"
 	softwareName: string; //e.g. "Figurenotes Composer"
 	softwareVersion: string; //e.g. "1.0.0"
-}
-export interface ScoreSettings {
-	divisionsPerQuarter: number; //e.g. 12
 }
 export interface NoteModel {
 	id: number; //internal
 	name?: string; //e.g. F#4
 	isRest?: boolean; //e.g. false
 	startDiv: number; //e.g. 0
-	durationDivs: number; //e.g. 12
+	durationDivs: number; //e.g. 24
 	isTiedToNext?: boolean; //e.g. false
 	isTiedToPrev?: boolean; //e.g. false
 }
@@ -22,7 +19,7 @@ export interface ChordModel {
 	name?: string; //e.g. Am
 	isRest?: boolean; //e.g. false
 	starDivs: number; //e.g. 0
-	durationDivs: number; //e.g. 48
+	durationDivs: number; //e.g. 96
 }
 export enum VoiceType {
 	FN_LVL_1 = 'FN_LVL_1',
@@ -42,10 +39,10 @@ export interface VoiceModel {
 export interface MeasureModel {
 	id: number; //public
 	isPickup?: boolean; //e.g. false
-	scale?: string; //e.g. "Am"
+	musicalScale?: string; //e.g. "Am"
 	timeSignature: string; //e.g. "4/4"
-	tempo?: number; //e.g. 120
-	durationDivs: number; //e.g. 48
+	tempoBpm?: number; //e.g. 120
+	durationDivs: number; //e.g. 96
 	voices: VoiceModel[];
 }
 export interface PartModel {
@@ -57,6 +54,5 @@ export interface ScoreModel {
 	version: number; //internal number
 	id: number; //internal ID
 	info: ScoreInfoModel;
-	settings: ScoreSettings;
 	parts: PartModel[];
 }
