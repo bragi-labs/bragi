a8087f64d71c5d7477662886494d3cab232e4d15
kalish <uri.kalish@gmail.com>
2021-06-17 16:28:25 +0300

    reduce default stage width

--- client/src/hooks/useSettingsContext.ts
+++ client/src/hooks/useSettingsContext.ts
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { createContainer } from 'unstated-next';
 
 const useSettingsContext = () => {
-	const [stageWidthCm, setStageWidthCm] = useState(19.0);
+	const [stageWidthCm, setStageWidthCm] = useState(18.9);
 	const [rowGapCm, setRowGapCm] = useState(1.0);
 	const [totalDurationDivsPerRow] = useState(24 * 4 * 4);
 
