a74616fe0a5abd436e3de4cbb541feedd5555c02
kalish <uri.kalish@gmail.com>
2021-06-25 18:06:21 +0300

    use static synth

--- client/src/services/soundHelper.ts
+++ client/src/services/soundHelper.ts
@@ -2,11 +2,12 @@ import * as Tone from 'tone';
 import { Synth } from 'tone';
 
 export class SoundHelper {
+	static synth = new Tone.PolySynth().toDestination();
 	static playShortNote(noteFullName: string, synth?: Synth) {
 		if (synth) {
 			synth.triggerAttackRelease(noteFullName, 0.1);
 		} else {
-			new Tone.Synth().toDestination().triggerAttackRelease(noteFullName, 0.1);
+			SoundHelper.synth.triggerAttackRelease(noteFullName, 0.1);
 		}
 	}
 	static startNote(noteFullName: string, synth: Synth) {
