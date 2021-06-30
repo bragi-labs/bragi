a39c8c9f109541e5e180f4a4e780a717155d695b
kalish <uri.kalish@gmail.com>
2021-07-01 00:44:14 +0300

    default size settings

--- client/src/hooks/useSettingsContext.ts
+++ client/src/hooks/useSettingsContext.ts
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import { createContainer } from 'unstated-next';
 
 const useSettingsContext = () => {
-	const [pageWidth, setPageWidth] = useState(793);
-	const [stageWidth, setStageWidth] = useState(714);
-	const [partsWidth, setPartsWidth] = useState(714);
-	const [quarterSize, setQuarterSize] = useState(36);
-	const [rowGap, setRowGap] = useState(36);
+	const [pageWidth, setPageWidth] = useState(776);
+	const [stageWidth, setStageWidth] = useState(718);
+	const [partsWidth, setPartsWidth] = useState(718); //718 and less
+	const [quarterSize, setQuarterSize] = useState(36); //20 to 44
+	const [rowGap, setRowGap] = useState(36); //20 to 80
 
 	return {
 		pageWidth,
