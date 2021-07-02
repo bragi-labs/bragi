ac2d022248e0915d0e885f9af745a82104c26228
kalish <uri.kalish@gmail.com>
2021-07-02 20:23:27 +0300

    note panel layout

--- client/src/views/composer/NotePanel.tsx
+++ client/src/views/composer/NotePanel.tsx
@@ -33,7 +33,7 @@ export const NotePanel = ({ score, onUpdateScore }: NotePanelProps) => {
 		},
 		content: {
 			display: 'grid',
-			gridTemplate: 'auto auto auto / 1fr',
+			gridTemplate: 'auto auto / 1fr',
 			gap: '24px 0',
 			backgroundColor: '#444',
 			padding: 24,
@@ -51,6 +51,16 @@ export const NotePanel = ({ score, onUpdateScore }: NotePanelProps) => {
 			'&:first-of-type': {
 				marginLeft: 0,
 			},
+		},
+		panelDuration: {
+			paddingRight: 4,
+			'& .MuiButton-root': {
+				minWidth: 56,
+			},
+		},
+		buttonsRow: {
+			display: 'flex',
+			justifyContent: 'space-between',
 		},
 		panelText: {
 			marginLeft: 4,
@@ -302,55 +312,55 @@ export const NotePanel = ({ score, onUpdateScore }: NotePanelProps) => {
 		<Box id="NotePanel" className={classes.root} style={{ left: `${position.x}px`, top: `${position.y}px`, zIndex: draggedItem === DraggedItem.NOTE_PANEL ? 100 : 10 }}>
 			<DraggablePanel onDragStart={handleDragStart} onDragMove={handleDragMove} onDragEnd={handleDragEnd} />
 			<Box className={classes.content}>
-				<Box>
-					<Box className={classes.panel}>
-						{noteDurationOptions.map((o, i) => (
-							<Button
-								key={i}
-								data-duration-divs={o.durationDivs}
-								onClick={handleClickNoteDuration}
-								disabled={!canChangeDuration[o.durationDivs]}
-								className={`${classes.actionButton} ${classes.noteDurationButton} ${canChangeDuration[o.durationDivs] ? '' : 'disabled'} ${
-									o.durationDivs === curDuration ? 'current' : ''
-								}`}
-							>
-								{o.label}
-							</Button>
-						))}
-					</Box>
+				<Box className={`${classes.panel} ${classes.panelDuration}`}>
+					{noteDurationOptions.map((o, i) => (
+						<Button
+							key={i}
+							data-duration-divs={o.durationDivs}
+							onClick={handleClickNoteDuration}
+							disabled={!canChangeDuration[o.durationDivs]}
+							className={`${classes.actionButton} ${classes.noteDurationButton} ${canChangeDuration[o.durationDivs] ? '' : 'disabled'} ${
+								o.durationDivs === curDuration ? 'current' : ''
+							}`}
+						>
+							{o.label}
+						</Button>
+					))}
 				</Box>
-				<Box>
-					<Box className={classes.panel}>
-						<IconButton onClick={handleChangePitch} data-direction="down" data-amount="semitone" className={`${classes.actionButton}`} disabled={!canPitchDown}>
-							<ArrowDownwardIcon titleAccess="Pitch Down" />
-						</IconButton>
-						<IconButton onClick={handleChangePitch} data-direction="up" data-amount="semitone" className={`${classes.actionButton}`} disabled={!canPitchUp}>
-							<ArrowUpwardIcon titleAccess="Pitch Up" />
-						</IconButton>
-						<Typography variant="body1" className={`${classes.panelText} ${canPitchUp || canPitchDown ? '' : 'disabled'}`}>
-							Semitone
-						</Typography>
+				<Box className={classes.buttonsRow}>
+					<Box>
+						<Box className={classes.panel}>
+							<IconButton onClick={handleChangePitch} data-direction="down" data-amount="semitone" className={`${classes.actionButton}`} disabled={!canPitchDown}>
+								<ArrowDownwardIcon titleAccess="Pitch Down" />
+							</IconButton>
+							<IconButton onClick={handleChangePitch} data-direction="up" data-amount="semitone" className={`${classes.actionButton}`} disabled={!canPitchUp}>
+								<ArrowUpwardIcon titleAccess="Pitch Up" />
+							</IconButton>
+							<Typography variant="body1" className={`${classes.panelText} ${canPitchUp || canPitchDown ? '' : 'disabled'}`}>
+								Semitone
+							</Typography>
+						</Box>
+						<Box className={classes.panel}>
+							<IconButton onClick={handleChangePitch} data-direction="down" data-amount="octave" className={`${classes.actionButton}`} disabled={!canOctaveDown}>
+								<ArrowDownwardIcon titleAccess="Octave Down" />
+							</IconButton>
+							<IconButton onClick={handleChangePitch} data-direction="up" data-amount="octave" className={`${classes.actionButton}`} disabled={!canOctaveUp}>
+								<ArrowUpwardIcon titleAccess="Octave Up" />
+							</IconButton>
+							<Typography variant="body1" className={`${classes.panelText} ${canPitchUp || canPitchDown ? '' : 'disabled'}`}>
+								Octave
+							</Typography>
+						</Box>
 					</Box>
-					<Box className={classes.panel}>
-						<IconButton onClick={handleChangePitch} data-direction="down" data-amount="octave" className={`${classes.actionButton}`} disabled={!canOctaveDown}>
-							<ArrowDownwardIcon titleAccess="Octave Down" />
-						</IconButton>
-						<IconButton onClick={handleChangePitch} data-direction="up" data-amount="octave" className={`${classes.actionButton}`} disabled={!canOctaveUp}>
-							<ArrowUpwardIcon titleAccess="Octave Up" />
-						</IconButton>
-						<Typography variant="body1" className={`${classes.panelText} ${canPitchUp || canPitchDown ? '' : 'disabled'}`}>
-							Octave
-						</Typography>
-					</Box>
-				</Box>
-				<Box>
-					<Box className={classes.panel}>
-						<IconButton onClick={handleClickDelete} className={`${classes.actionButton}`} disabled={!canDelete}>
-							<DeleteForeverIcon titleAccess="Delete" />
-						</IconButton>
-						<Typography onClick={handleClickDelete} variant="body1" className={`${classes.panelText} clickable ${canDelete ? '' : 'disabled'}`}>
-							Delete
-						</Typography>
+					<Box>
+						<Box className={classes.panel}>
+							<IconButton onClick={handleClickDelete} className={`${classes.actionButton}`} disabled={!canDelete}>
+								<DeleteForeverIcon titleAccess="Delete" />
+							</IconButton>
+							<Typography onClick={handleClickDelete} variant="body1" className={`${classes.panelText} clickable ${canDelete ? '' : 'disabled'}`}>
+								Delete
+							</Typography>
+						</Box>
 					</Box>
 				</Box>
 			</Box>
