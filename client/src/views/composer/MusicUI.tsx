b933e00a411989921513bb486c82194f986ccccc
kalish <uri.kalish@gmail.com>
2021-07-07 01:40:37 +0300

    divide musicUI into parts - text part

--- client/src/views/composer/MusicUI.tsx
+++ client/src/views/composer/MusicUI.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useMemo } from 'react';
-import { useRecoilState, useResetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
-import { TextField, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 import { MusicalHelper } from '../../services/musicalHelper';
 import { SoundHelper } from '../../services/soundHelper';
@@ -11,6 +11,7 @@ import { MusicModel, PartType } from '../../model/scoreModel';
 import { ScoreSettings } from '../../model/scoreSettings';
 import { Music } from '../../model/music';
 import { selectionAtom } from '../../atoms/selectionAtom';
+import { PartTextUI } from './PartTextUI';
 
 export interface MusicUIProps {
 	music: MusicModel;
@@ -90,81 +91,10 @@ export const MusicUI = ({ music, scoreSettings }: MusicUIProps) => {
 			},
 			zIndex: -1,
 		},
-		textLine: {
-			display: 'flex',
-			width: '100%',
-			'& .MuiTextField-root': {
-				width: '100%',
-				'& .MuiInput-formControl': {
-					width: '100%',
-					'& .MuiInput-input': {
-						width: '100%',
-						padding: 2,
-						fontFamily: 'Arial, sans-serif',
-						color: '#000',
-					},
-				},
-				'&.font-weight-bold .MuiInput-input': {
-					fontWeight: 900,
-				},
-				'&.textSize-8 .MuiInput-input': {
-					fontSize: '8px',
-				},
-				'&.textSize-9 .MuiInput-input': {
-					fontSize: '9px',
-				},
-				'&.textSize-10 .MuiInput-input': {
-					fontSize: '10px',
-				},
-				'&.textSize-11 .MuiInput-input': {
-					fontSize: '11px',
-				},
-				'&.textSize-12 .MuiInput-input': {
-					fontSize: '12px',
-				},
-				'&.textSize-13 .MuiInput-input': {
-					fontSize: '13px',
-				},
-				'&.textSize-14 .MuiInput-input': {
-					fontSize: '14px',
-				},
-				'&.textSize-15 .MuiInput-input': {
-					fontSize: '15px',
-				},
-				'&.textSize-16 .MuiInput-input': {
-					fontSize: '16px',
-				},
-				'&.textSize-17 .MuiInput-input': {
-					fontSize: '17px',
-				},
-				'&.textSize-18 .MuiInput-input': {
-					fontSize: '18px',
-				},
-				'&.textSize-19 .MuiInput-input': {
-					fontSize: '19px',
-				},
-				'&.textSize-20 .MuiInput-input': {
-					fontSize: '20px',
-				},
-				'&.textSize-21 .MuiInput-input': {
-					fontSize: '21px',
-				},
-				'&.textSize-22 .MuiInput-input': {
-					fontSize: '22px',
-				},
-				'&.textSize-23 .MuiInput-input': {
-					fontSize: '23px',
-				},
-				'&.textSize-24 .MuiInput-input': {
-					fontSize: '24px',
-				},
-			},
-		},
 	}));
 	const classes = useStyles();
 
 	const [selection, setSelection] = useRecoilState(selectionAtom);
-	const resetSelection = useResetRecoilState(selectionAtom);
 
 	const sizeVars = useMemo(() => {
 		const exampleMeasure = music.measures[0].isPickup ? music.measures[1] : music.measures[0];
@@ -196,34 +126,9 @@ export const MusicUI = ({ music, scoreSettings }: MusicUIProps) => {
 		[music, setSelection],
 	);
 
-	const handleTextFocus = useCallback(
-		(e) => {
-			const p = Music.findPart(music, e.target.parentElement.parentElement.dataset.partId);
-			if (p) {
-				const partInfoId = e.target.parentElement.parentElement.dataset.partInfoId;
-				setSelection([{ partInfoId, measureId: p.measureId, partId: p.id, noteId: '' }]);
-			}
-		},
-		[music, setSelection],
-	);
-
-	const handleTextChange = useCallback(
-		(e) => {
-			const p = Music.findPart(music, e.target.parentElement.parentElement.dataset.partId);
-			if (p) {
-				p.text = e.target.value;
-			}
-		},
-		[music],
-	);
-
-	const handleTextBlur = useCallback(() => {
-		resetSelection();
-	}, [resetSelection]);
-
 	const getPartInfo = useCallback(
 		(partInfoId: string) => {
-			return music.partsInfo.find((pi) => pi.id === partInfoId);
+			return music.partsInfo.find((pi) => pi.id === partInfoId) || null;
 		},
 		[music],
 	);
@@ -232,22 +137,6 @@ export const MusicUI = ({ music, scoreSettings }: MusicUIProps) => {
 		(partInfoId: string) => {
 			const pi = getPartInfo(partInfoId);
 			return pi ? pi.fontSize : 0;
-		},
-		[getPartInfo],
-	);
-
-	const isPartInfoBold = useCallback(
-		(partInfoId: string) => {
-			const pi = getPartInfo(partInfoId);
-			return pi ? pi.isBold : false;
-		},
-		[getPartInfo],
-	);
-
-	const getPartInfoBgColor = useCallback(
-		(partInfoId: string) => {
-			const pi = getPartInfo(partInfoId);
-			return pi ? pi.bgColor : '#f6f6f6';
 		},
 		[getPartInfo],
 	);
@@ -364,20 +253,7 @@ export const MusicUI = ({ music, scoreSettings }: MusicUIProps) => {
 													</Box>
 												))}
 											{p.partType === PartType.TEXT && (
-												<Box className={classes.textLine} style={{ backgroundColor: `${getPartInfoBgColor(p.partInfoId)}` }}>
-													<TextField
-														data-part-info-id={p.partInfoId}
-														data-measure-id={m.id}
-														data-part-id={p.id}
-														defaultValue={p.text}
-														onFocus={handleTextFocus}
-														onChange={handleTextChange}
-														onBlur={handleTextBlur}
-														label=""
-														className={`textSize-${getPartInfoFontSize(p.partInfoId)} ${isPartInfoBold(p.partInfoId) ? 'font-weight-bold' : ''}`}
-														style={{ borderBottom: `${pIndex === m.parts.length - 1 ? 0 : 1}px solid #eee` }}
-													/>
-												</Box>
+												<PartTextUI partInfo={getPartInfo(p.partInfoId)} part={p} isLastPart={pIndex === m.parts.length - 1} />
 											)}
 										</Box>
 									)}
