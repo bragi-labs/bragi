7f1b1e483ff40607a87198a97253aa61f70c5d4f
kalish <uri.kalish@gmail.com>
2022-03-14 21:13:17 +0200

    add about info

--- client/src/views/about/AboutPage.tsx
+++ client/src/views/about/AboutPage.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
-import { UnderConstruction } from '../../components/UnderConstruction';
 
 export const AboutPage = React.memo(() => {
 	const useStyles = makeStyles(() => ({
 		root: {
 			position: 'relative',
 			height: '100%',
+			display: 'grid',
+			placeItems: 'center',
 			userSelect: 'none',
+		},
+		text: {
+			margin: '0 auto',
+			fontSize: 32,
+			color: '#fff',
+			textShadow: '2px 2px #000',
 		},
 	}));
 	const classes = useStyles();
 
 	return (
 		<Box id="AboutPage" className={classes.root}>
-			<UnderConstruction />
+			<Box className={classes.text}>Figurenotes Composer by Uri Kalish</Box>
 		</Box>
 	);
 });
