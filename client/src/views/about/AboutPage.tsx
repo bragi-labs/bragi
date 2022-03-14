e4dc9b4e5634ad7b789c165c84f3baf45ab162c9
kalish <uri.kalish@gmail.com>
2022-03-14 21:54:23 +0200

    add about info

--- client/src/views/about/AboutPage.tsx
+++ client/src/views/about/AboutPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
+import { AppDataHelper } from '../../services/appDataHelper';
 
 export const AboutPage = React.memo(() => {
 	const useStyles = makeStyles(() => ({
@@ -11,10 +12,21 @@ export const AboutPage = React.memo(() => {
 			placeItems: 'center',
 			userSelect: 'none',
 		},
+		container: {
+			border: '1px solid #666',
+			borderRadius: 8,
+			padding: 24,
+			backgroundColor: '#222',
+			opacity: 0.75,
+			fontSize: 24,
+		},
 		text: {
-			margin: '0 auto',
-			fontSize: 32,
 			color: '#fff',
+			textShadow: '2px 2px #000',
+		},
+		link: {
+			color: '#fa3',
+			textDecoration: 'none',
 			textShadow: '2px 2px #000',
 		},
 	}));
@@ -22,7 +34,22 @@ export const AboutPage = React.memo(() => {
 
 	return (
 		<Box id="AboutPage" className={classes.root}>
-			<Box className={classes.text}>Figurenotes Composer by Uri Kalish</Box>
+			<Box className={classes.container}>
+				<Box className={classes.text}>
+					{AppDataHelper.appName} {AppDataHelper.appVersion}
+				</Box>
+				<Box className={classes.text}>
+					Developed by Uri Kalish for{' '}
+					<a href="https://www.imagine.org.il/" target="_blank" className={classes.link}>
+						imagine.org.il
+					</a>
+				</Box>
+				<Box>
+					<a href="mailto:uri.kalish@gmail.com" className={classes.link}>
+						uri.kalish@gmail.com
+					</a>
+				</Box>
+			</Box>
 		</Box>
 	);
 });
